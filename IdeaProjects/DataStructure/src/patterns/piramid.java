c2dd50b6a45bf15faa5fef8f4a7a024c26469c3e
regno01 <212147545+regno001@users.noreply.github.com>
2026-01-13 22:53:26 +0530

    piramid pattern

---
+++ IdeaProjects/DataStructure/src/patterns/piramid.java
@@ -0,0 +1,17 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class piramid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of line that you want to print ");
+        int n = input.nextInt();
+        for(int i=0 ; i<n;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
