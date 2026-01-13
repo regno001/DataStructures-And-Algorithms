4eedd3441cff12165726c47269d861ac64518977
regno01 <212147545+regno001@users.noreply.github.com>
2026-01-13 23:09:17 +0530

    mirror half inverted pyramid

---
+++ IdeaProjects/DataStructure/src/patterns/MirredHlafPyramid.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class MirredHlafPyramid {
+    public static void main(String[] args) {
+        System.out.println("Enter the num of row ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i=0;i<n;i++){
+            for (int j=0;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
