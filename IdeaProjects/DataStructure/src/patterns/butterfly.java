ca8b6c0505a70872c4a3e312a74af49676441ee2
regno01 <212147545+regno001@users.noreply.github.com>
2026-01-14 21:32:18 +0530

    butterfly

---
+++ IdeaProjects/DataStructure/src/patterns/butterfly.java
@@ -0,0 +1,37 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class butterfly {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of row");
+        int n= input.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++) {
+                System.out.print("*"+ " ");
+            }
+            int space =2 *(n-i);
+            for (int j=1;j<=space ; j++){
+                System.out.print(" "+ " ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*"+ " ");
+            }
+            System.out.println(" ");
+        }
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=i;j++) {
+                System.out.print("*"+ " ");
+            }
+            int space =2 *(n-i);
+            for (int j=1;j<=space ; j++){
+                System.out.print(" "+ " ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*"+ " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
