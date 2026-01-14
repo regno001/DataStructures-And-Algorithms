09ba78fc7583fcc597b1a806580bd969916c6ffb
regno01 <212147545+regno001@users.noreply.github.com>
2026-01-14 19:38:18 +0530

    floyd triangle

---
+++ IdeaProjects/DataStructure/src/patterns/floydTriangle.java
@@ -0,0 +1,19 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class floydTriangle {
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the number of line that you want to print ");
+            int n = input.nextInt();
+            int num =1;
+            for (int i=1;i<=n;i++){
+                for (int j=1;j<=i;j++){
+                    System.out.print(num+ " ");
+                    num++;
+                }
+                System.out.println(" ");
+        }
+    }
+}
