39fd62918c060b0920974460c6cc23266bb5056e
regno01 <212147545+regno001@users.noreply.github.com>
2026-01-14 21:59:12 +0530

    number pyramid

---
+++ IdeaProjects/DataStructure/src/patterns/numberPyramid.java
@@ -0,0 +1,22 @@
+package patterns;
+
+import java.util.Scanner;
+
+
+public class numberPyramid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Length");
+        int n = input.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" "+" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print(i +" ");
+                System.out.print(" "+" " );
+            }
+            System.out.println(" ");
+        }
+    }
+    }
